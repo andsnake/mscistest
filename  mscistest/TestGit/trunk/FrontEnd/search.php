a39a2c9343d6466abbb6b3ba884bf304e950da15
antoniougeocy@gmail.com <antoniougeocy@gmail.com@54f5fc16-f106-7db3-75e1-b4194d2e7d61>
2014-06-28 17:35:16 +0000

    third commit to cvs.. working(smi-working): -add to cart 100% -remove from cart 100% -categories products 100% -login/logout 100% -register 100% -Checkout 100% ------------------------------TO DO-------------------------------- -Search  -Recommender

---
+++  mscistest/TestGit/trunk/FrontEnd/search.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: George
+ * Date: 6/28/14
+ * Time: 8:24 PM
+ */
+
+if(isset($_GET['mode'])){
+    //to do search handling
+    if($_GET['mode']=="product"){
+
+    }//return products that belong to a certain category
+    elseif($_GET['mode']=="category"){
+
+    }elseif($_GET['mode']=="mixed"){
+
+    }
+}
